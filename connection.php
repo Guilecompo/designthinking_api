a79d7f0da9d2b23782cc428b125cab6da1c1f8d9
Guilecompo <kideguilecompo03@gmail.com>
2024-09-25 23:23:12 +0800

    zxczxcasd

--- connection.php
+++ connection.php
@@ -1,18 +1,19 @@
 <?php
- header('Content-Type: application/json');
- header("Access-Control-Allow-Origin: *");
+header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: https://flutter-login-api.netlify.app"); // Specify your Netlify domain
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
 
-  $host = '152.42.243.189';
-  $dbusername = 'waterworks';
-  $dbpassword = 'Waterworks2024!';
-  $database = 'flutter_login';
+$host = '152.42.243.189';
+$dbusername = 'waterworks';
+$dbpassword = 'Waterworks2024!';
+$database = 'flutter_login';
 
-  try {
+try {
     $conn = new PDO("mysql:host=$host;dbname=$database", $dbusername, $dbpassword);
-    // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     // echo "Connection Successful!";
-  } catch(PDOException $e) {
-    die("Error: " . $e->getMessage());
-  }
+} catch(PDOException $e) {
+    die(json_encode(["error" => "Error: " . $e->getMessage()]));
+}
 ?>
